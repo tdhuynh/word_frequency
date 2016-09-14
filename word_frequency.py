179019998a40f7fd8e3d7e88cd0a94f520bf5837
tdhuynh <tommyhuynh93@gmail.com>
2016-09-14 15:44:47 -0400

    sorted words by count

--- word_frequency.py
+++ word_frequency.py
@@ -1,10 +1,16 @@
 
-exclude = ",'.\"[]()!?/:$-@*"
+from string import punctuation
 
 with open("sample.txt") as open_file:
     book = open_file.read().lower()
-    for item in exclude:
+    for item in punctuation:
         book = book.replace(item, "")
 
-
-print(book)
+    book_list = book.split()
+    book_dict = {}
+    for word in book_list:
+        if word in book_dict:
+            book_dict[word] += 1
+        else:
+            book_dict[word] = 1
+    print(sorted(book_dict.items(), key=lambda x: x[1]))
