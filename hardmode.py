79cd8c2e36625ff1d297efe90c6c5ffe889377e8
tdhuynh <tommyhuynh93@gmail.com>
2016-09-15 09:58:59 -0400

    attempted to finish hard mode

--- hardmode.py
+++ hardmode.py
@@ -5,33 +5,26 @@ with open("sample.txt") as open_file:
     book = open_file.read().lower()
 
 for item in punctuation:
-    book = book.replace(item, "")
+    book = book.replace(item, " ")
 
 ignore = " a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your "
 for comma in punctuation:
     ignore = ignore.replace(comma, " , ")
 
-ignore = tuple(ignore.split(","))
+ignore = ignore.split(",")
 
-# for i in ignore:
-#     book = book.replace(i, " ")
-
-print(book)
+for i in ignore:
+    book = book.replace(i, " ")
 
 book_list = book.split()
-#    for i in ignore:
-#        book_list = book_list.remove(i)
 
+book_dict = {}
+for word in book_list:
+    if word in book_dict:
+        book_dict[word] += 1
+    else:
+        book_dict[word] = 1
+book_sort = sorted(book_dict.items(), key=lambda x: x[1])
+book_sort = book_sort[:-21:-1]
 
-#    print(book_list)
-#     book_dict = {}
-#     for word in book_list:
-#         if word in book_dict:
-#             book_dict[word] += 1
-#         else:
-#             book_dict[word] = 1
-#     book_sort = sorted(book_dict.items(), key=lambda x: x[1])
-#     book_sort = book_sort[:-21:-1]
-#
-# for item in book_sort:
-#     print(item)
+print(book_sort)
--- word_frequency.py
+++ word_frequency.py
@@ -3,18 +3,19 @@ from string import punctuation
 
 with open("sample.txt") as open_file:
     book = open_file.read().lower()
-    for item in punctuation:
-        book = book.replace(item, "")
+    
+for item in punctuation:
+    book = book.replace(item, "")
 
-    book_list = book.split()
-    book_dict = {}
-    for word in book_list:
-        if word in book_dict:
-            book_dict[word] += 1
-        else:
-            book_dict[word] = 1
-    book_sort = sorted(book_dict.items(), key=lambda x: x[1])
-    book_sort = book_sort[:-21:-1]
+book_list = book.split()
+book_dict = {}
+for word in book_list:
+    if word in book_dict:
+        book_dict[word] += 1
+    else:
+        book_dict[word] = 1
+book_sort = sorted(book_dict.items(), key=lambda x: x[1])
+book_sort = book_sort[:-21:-1]
 
 for a, b in book_sort:
     print("{} {}".format(a, b))
